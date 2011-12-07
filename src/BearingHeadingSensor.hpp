0955f15284ad0633b1bda42847c013ed49068f01
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-12-07 12:34:43 +0100

    add missing headers in  the bearing-heading-sensor definition

--- src/BearingHeadingSensor.hpp
+++ src/BearingHeadingSensor.hpp
@@ -1,6 +1,9 @@
 #ifndef ROCK_TUTORIAL_BEARING_HEADING_SENSOR
 #define ROCK_TUTORIAL_BEARING_HEADING_SENSOR
 
+#include <base/time.h>
+#include <base/angle.h>
+
 namespace rock_tutorial
 {
     struct BearingHeadingSensor
