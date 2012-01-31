e82aae920b07d1427b9540dcce98d277b0d4fc59
Felix Rehrmann <felix.rehrmann@dfki.de>
2012-01-31 15:43:53 +0100

    fix registration of the pose visualization on vizkit3d

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,3 +1,3 @@
 Vizkit::UiLoader.register_3d_plugin('RockViz', 'rock_tutorial', 'RockVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('RockViz', "/base/Pose_m", :updateRigidBodyState)
+Vizkit::UiLoader.register_3d_plugin_for('RockViz', "/base/Pose_m", :updatePose)
 
