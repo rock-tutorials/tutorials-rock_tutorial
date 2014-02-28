a058053fb59252b2d5af1776defbef8b286374b8
Thomas Roehr <thomas.roehr@dfki.de>
2014-02-28 10:07:44 +0100

    Adapt to CamelCase headers

--- src/BearingDistanceSensor.hpp
+++ src/BearingDistanceSensor.hpp
@@ -1,8 +1,8 @@
 #ifndef ROCK_TUTORIAL_BEARING_HEADING_SENSOR
 #define ROCK_TUTORIAL_BEARING_HEADING_SENSOR
 
-#include <base/time.h>
-#include <base/angle.h>
+#include <base/Time.hpp>
+#include <base/Angle.hpp>
 
 namespace rock_tutorial
 {
